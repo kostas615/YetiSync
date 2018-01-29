971c7e721425520ed8d14771105f91a09049b986
Konstantinos Chatzis <kostaschatzis01@gmail.com>
2018-01-29 18:48:33 +0200

    Added D7 Support and leads table

--- Drupal/7/yetisync/yeti-api/api-get-json.php
+++ Drupal/7/yetisync/yeti-api/api-get-json.php
@@ -131,6 +131,45 @@
 			return 0;
 		}
 
+
+		public function getLeads () {
+			try {
+				// Import API Settings
+				include ('api-config.php');
+
+				// Get a current token for the API
+				$token = $this->getToken();
+
+				// Get Contacts
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL,$get_leads_url);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+				curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+				curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+					"x-api-key: $api_key",
+					"x-token: $token",
+					"x-encrypted: 0",
+					"x-fields: [\"accountname\",\"email\",\"phone\",\"addresslevel8a\",\"addresslevel7a\",\"addresslevel5a\",\"addresslevel3a\"]"
+					)
+				);
+				$curl_response = curl_exec($ch);
+				curl_close($ch);
+
+				// Load decoded response
+				$response = json_decode($curl_response);
+
+				return $response;
+
+			}
+			catch (\Exception $e){
+	      echo 'Caught exception: ',  $e->getMessage(), "\n";
+	    }
+
+			return 0;
+		}
+
 	}
 
 ?>
